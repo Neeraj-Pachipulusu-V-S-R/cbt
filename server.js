f0caf81bdad615996f89aa1917e78749c66a2187
NEERAJ P V S R <96047207+Neeraj-Pachipulusu-V-S-R@users.noreply.github.com>
2024-04-11 18:34:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
 const MODEL_NAME = "gemini-pro";
-const API_KEY = process.env.API_KEY;
+const API_KEY = AIzaSyDxK7CxvKYGt62fL3XsLJDR6oB-cXdX35I;
 
 async function runChat(userInput) {
   const genAI = new GoogleGenerativeAI(API_KEY);
